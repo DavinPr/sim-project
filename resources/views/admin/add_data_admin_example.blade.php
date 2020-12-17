a87c15d6574c7c8ac26adebbf2bba52a7e9a1ee9
luhurfatah <lm.fatah@gmail.com>
2020-12-18 02:43:55 +0700

    Pembaruan Tampilan Log in,  Admin dan Santri

--- resources/views/admin/add_data_admin_example.blade.php
+++ resources/views/admin/add_data_admin_example.blade.php
@@ -1,54 +1,145 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Laravel</title>
 
-    <title>Hello, world!</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="https://kit.fontawesome.com/543ccfb2d9.js" crossorigin="anonymous"></script>
+
+
 </head>
 
 <body>
-    <div class="container mt-4">
-        <form method="POST" action="{{ route('admin.create.admin',) }}">
-            @csrf
-            <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" name="username">
+
+    <div class="container-fluid wrapper">
+        <div class="row">
+
+            <div class="col-3 sidebar">
+                <div class="container">
+                    <div class="logo d-flex justify-content-center">
+                        <img src="/img/logo.png" alt="" srcset="">
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="menus">
+
+                        <ul class="menus-block">
+                            <li>
+                                <a class="menu-link" href="{{route('admin.homepage')}}">
+                                    <span class="icon fas fa-home"></span>
+                                    Beranda
+                                </a>
+                            </li>
+                            <li>
+                                <a class="menu-link" href="#">
+                                    <span class="icon fas fa-user-circle"></span>
+                                    Akun
+                                </a>
+                            </li>
+                            <li>
+                                <a class="menu-link" href="#">
+                                    <span class="icon fas fa-history"></span>
+                                    Transaksi
+                                </a>
+                            </li>
+
+                            <li>
+                                <a class="menu-link" href="#">
+                                    <span class="icon fas fa-check-square"></span>
+                                    Verifikasi
+                                </a>
+                            </li>
+
+                            <li>
+                                <a class="menu-link" href="#">
+                                    <span class="icon fas fa-file-alt"></span>
+                                    Laporan
+                                </a>
+                            </li>
+
+
+
+                        </ul>
+
+                    </div>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" , name="password">
+
+
+            <div class="col-9 main-wrapper">
+                <div class="container">
+                    <ul class="nav justify-content-end nav-top">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <span class="nav-icon fas fa-bell"></span>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span class="nav-icon fas fa-cog"></span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+
+                <div class="container list-santri">
+                    <div class="container mt-4">
+                        <form method="POST" action="{{ route('admin.create.admin',) }}">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" class="form-control" id="username" name="username">
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="password" , name="password">
+                            </div>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Nama Lengkap</label>
+                                <input type="text" class="form-control" id="name" name="person_name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="birthdate" class="form-label">Tanggal Lahir</label>
+                                <input type="date" class="form-control" id="birthdate" name="person_birthdate">
+                            </div>
+                            <div class="mb-3">
+                                <label for="birthplace" class="form-label">Tempat Lahir</label>
+                                <input type="text" class="form-control" id="birthplace" name="person_birthplace">
+                            </div>
+                            <div class="mb-3">
+                                <label for="gender" class="form-label">Jenis Kelamin</label>
+                                <br>
+
+                                <input type="radio" id="male" name="person_gender" value="Laki-laki">
+                                <label for="male">Laki-laki</label><br>
+                                <input type="radio" id="female" name="person_gender" value="Perempuan">
+                                <label for="female">Perempuan</label><br>
+                            </div>
+                            <div class="mb-3">
+                                <label for="avatar" class="form-label">Avatar</label>
+                                <input type="text" class="form-control" id="avatar" name="person_avatar">
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+
             </div>
-            <div class="mb-3">
-                <label for="name" class="form-label">Nama Lengkap</label>
-                <input type="text" class="form-control" id="name" name="person_name">
-            </div>
-            <div class="mb-3">
-                <label for="birthdate" class="form-label">Tanggal Lahir</label>
-                <input type="date" class="form-control" id="birthdate" name="person_birthdate">
-            </div>
-            <div class="mb-3">
-                <label for="birthplace" class="form-label">Tempat Lahir</label>
-                <input type="text" class="form-control" id="birthplace" name="person_birthplace">
-            </div>
-            <div class="mb-3">
-                <label for="gender" class="form-label">Jenis Kelamin</label>
-                <input type="text" class="form-control" id="gender" name="person_gender">
-            </div>
-            <div class="mb-3">
-                <label for="avatar" class="form-label">Avatar</label>
-                <input type="text" class="form-control" id="avatar" name="person_avatar">
-            </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </form>
-    </div>
+        </div>
 </body>
 
 </html>
--- resources/views/admin/show_data_admin_example.blade.php
+++ resources/views/admin/show_data_admin_example.blade.php
@@ -1,60 +1,153 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Laravel</title>
 
-    <title>Hello, world!</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="https://kit.fontawesome.com/543ccfb2d9.js" crossorigin="anonymous"></script>
+
+
 </head>
 
 <body>
-    <div class="container mt-4">
-        @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
+
+    <div class="container-fluid wrapper">
+        <div class="row">
+
+            <div class="col-3 sidebar">
+                <div class="container">
+                    <div class="logo d-flex justify-content-center">
+                        <img src="/img/logo.png" alt="" srcset="">
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="menus">
+
+                        <ul class="menus-block">
+                            <li>
+                                <a class="menu-link" href="{{route('admin.homepage')}}">
+                                    <span class="icon fas fa-home"></span>
+                                    Beranda
+                                </a>
+                            </li>
+                            <li>
+                                <a class="menu-link" href="#">
+                                    <span class="icon fas fa-user-circle"></span>
+                                    Akun
+                                </a>
+                            </li>
+                            <li>
+                                <a class="menu-link" href="#">
+                                    <span class="icon fas fa-history"></span>
+                                    Transaksi
+                                </a>
+                            </li>
+
+                            <li>
+                                <a class="menu-link" href="#">
+                                    <span class="icon fas fa-check-square"></span>
+                                    Verifikasi
+                                </a>
+                            </li>
+
+                            <li>
+                                <a class="menu-link" href="#">
+                                    <span class="icon fas fa-file-alt"></span>
+                                    Laporan
+                                </a>
+                            </li>
+
+
+
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-9 main-wrapper">
+                <div class="container">
+                    <ul class="nav justify-content-end nav-top">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <span class="nav-icon fas fa-bell"></span>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span class="nav-icon fas fa-cog"></span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{route('logout')}}"><span class="nav-icon fas fa-sign-out-alt"></span></a>
+                            
+                        </li>
+
+                    </ul>
+                </div>
+
+                <div class="container list-santri">
+                    @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Tanggal lahir</th>
+                                <th scope="col">Tempat lahir</th>
+                                <th scope="col">Gender</th>
+                                <th>Detail</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($admins as $admin)
+                            <tr>
+                                <th scope="row">{{ $loop->iteration }}</th>
+                                <td>{{$admin->username}}</td>
+                                <td>{{$admin->person->person_name}}</td>
+                                <td>{{$admin->person->person_birthdate}}</td>
+                                <td>{{$admin->person->person_birthplace}}</td>
+                                <td>{{$admin->person->person_gender}}</td>
+                                <td><a href="{{route('admin.detail.admin', $admin->id)}}">Click</a></td>
+                                <td><a href="{{route('admin.edit.admin', $admin->id)}}">Click</a></td>
+                                <td>
+                                    <form action="/admin/{{$admin->person_id}}" method="POST">
+                                        @method('delete')
+                                        @csrf
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <a href="{{route('admin.add.admin.page')}}"> <button class="btn btn-primary">Tambahkan
+                            Admin</button></a>
+                </div>
+
+
+
+            </div>
+
         </div>
-        @endif
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Tanggal lahir</th>
-                    <th scope="col">Tempat lahir</th>
-                    <th scope="col">Gender</th>
-                    <th>Detail</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($admins as $admin)
-                <tr>
-                    <th scope="row">{{ $loop->iteration }}</th>
-                    <td>{{$admin->username}}</td>
-                    <td>{{$admin->person->person_name}}</td>
-                    <td>{{$admin->person->person_birthdate}}</td>
-                    <td>{{$admin->person->person_birthplace}}</td>
-                    <td>{{$admin->person->person_gender}}</td>
-                    <td><a href="{{route('admin.detail.admin', $admin->id)}}">Click</a></td>
-                    <td><a href="{{route('admin.edit.admin', $admin->id)}}">Click</a></td>
-                    <td>
-                        <form action="/admin/{{$admin->person_id}}" method="POST">
-                            @method('delete')
-                            @csrf
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
     </div>
 </body>
 
